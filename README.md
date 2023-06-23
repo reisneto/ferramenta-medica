7b2578f1aa4c12cab83553e149991f70352139b3
Álvaro Reis <reisneto.alvaro@gmail.com>
2023-06-23 22:07:56 +0000

    chore: add prettier and husky

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ferramenta-medica
+
 Uma plataforma com diversas ferramentas digitais voltadas a ajudar o dia a dia do médico
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 function Page() {
-    return <h1>Ferramentas Médicas (Em Construção)</h1>
+    return <h1>Ferramentas Médicas (Em Construção)</h1>;
 }
 
 export default Page;
