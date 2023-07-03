1c49338df97c5d7f72685e2b1505fbbba740ec89
Alvaro Reis <reisneto.alvaro@gmail.com>
2023-07-03 19:16:54 -0400

    feat: create api folder and initial route

---
+++ app/api/suggestions/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    return NextResponse.json({ data });
+}
