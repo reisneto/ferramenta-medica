b2ae0fad8e90611f1cbbd488463d59c7ccc36314
Alvaro Reis <reisneto.alvaro@gmail.com>
2023-07-09 16:48:17 -0400

    feat: add roboto font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { roboto } from '../styles/font';
 
 export const metadata = {
     title: 'Next.js',
@@ -7,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className={roboto.className}>
             <body>{children}</body>
         </html>
     );
