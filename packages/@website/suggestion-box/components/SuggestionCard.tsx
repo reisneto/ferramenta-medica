d06e829b314617739f83a6354e8f278d49f107d3
Álvaro Reis <reisneto.alvaro@gmail.com>
2023-09-24 12:14:42 -0400

    feat: add suggestion card structure

--- packages/@website/suggestion-box/components/SuggestionCard.tsx
+++ packages/@website/suggestion-box/components/SuggestionCard.tsx
@@ -1,5 +1,29 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
 export const SuggestionCard: React.FC = (props: any) => {
-    return <h1>Suggestion Card</h1>;
+    return (
+        <Container>
+            <h2>Suggestion Card</h2>
+            <p>
+                O teste médico TC6M avalia a capacidade funcional e tolerância
+                ao esforço em pacientes com doenças respiratórias,
+                cardiovasculares ou neuromusculares, por meio de uma caminhada
+                de 6 minutos. É usado para monitorar a progressão da doença e
+                ajustar o tratamento.
+            </p>
+            <span>❤️ 103</span>
+        </Container>
+    );
 };
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+
+    p {
+        text-align: justify;
+    }
+`;
