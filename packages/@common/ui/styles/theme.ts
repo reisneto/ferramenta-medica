d8996ef53890427be360cbc2c422bfde0c000bfd
Álvaro Reis <reisneto.alvaro@gmail.com>
2023-09-25 15:26:21 -0400

    feat: styling component

--- packages/@common/ui/styles/theme.ts
+++ packages/@common/ui/styles/theme.ts
@@ -5,6 +5,25 @@ export const theme = (mode: ThemeMode) => {
     return createTheme({
         palette: {
             mode,
+            primary: {
+                main: '#4682b4',
+                light: '#fff',
+                dark: '#000',
+            },
+            text: {
+                secondary: 'rgba(0,0,0,.8)',
+            },
+        },
+        typography: {
+            h1: {
+                fontSize: 32,
+            },
+            h2: {
+                fontSize: 24,
+            },
+            body1: {
+                fontSize: 16,
+            },
         },
     });
 };
--- packages/@website/suggestion-box/components/SuggestionCard.tsx
+++ packages/@website/suggestion-box/components/SuggestionCard.tsx
@@ -1,29 +1,35 @@
 import styled from '@emotion/styled';
+import { Paper, Typography } from '@mui/material';
 import React from 'react';
 
 export const SuggestionCard: React.FC = (props: any) => {
     return (
-        <Container>
-            <h2>Suggestion Card</h2>
-            <p>
+        <Container elevation={1}>
+            <Typography variant="h2">Formulário TC6M</Typography>
+            <TextBody variant="body1">
                 O teste médico TC6M avalia a capacidade funcional e tolerância
                 ao esforço em pacientes com doenças respiratórias,
                 cardiovasculares ou neuromusculares, por meio de uma caminhada
                 de 6 minutos. É usado para monitorar a progressão da doença e
                 ajustar o tratamento.
-            </p>
-            <span>❤️ 103</span>
+            </TextBody>
+            <Typography variant="body1">❤️ 87</Typography>
         </Container>
     );
 };
 
-const Container = styled.div`
+const Container = styled(Paper)`
     display: flex;
+    padding: 16px 10px;
     flex-direction: column;
     align-items: center;
     gap: 8px;
 
-    p {
-        text-align: justify;
-    }
+    border-radius: 4px;
+    background: ${(props) => props.theme.palette.primary.light};
+`;
+
+const TextBody = styled(Typography)`
+    text-align: justify;
+    color: ${(props) => props.theme.palette.text.secondary};
 `;
