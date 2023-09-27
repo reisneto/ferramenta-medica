b9ff0bcb51081f01327714fe328c13820850dff3
Álvaro Reis <reisneto.alvaro@gmail.com>
2023-09-27 10:04:53 -0400

    [ALV-41] create suggestion card (#36)
    * fix: add global styles to storybook
    
    * feat: add suggestion card structure
    
    * chore: add storybook
    
    * chore: upgrade storybook
    
    * feat: add theme provider to storybook
    
    * feat: styling component
    
    * feat: add props to card
    
    * feat: create suggestion list component
    
    * feat: show suggestions data
    
    * feat: style improvements
    
    * fix: suggestions endpoint's path

--- packages/@website/suggestion-box/components/SuggestionCard.tsx
+++ packages/@website/suggestion-box/components/SuggestionCard.tsx
@@ -1,5 +1,46 @@
+import styled from '@emotion/styled';
+import { Paper, Typography } from '@mui/material';
 import React from 'react';
 
-export const SuggestionCard: React.FC = (props: any) => {
-    return <h1>Suggestion Card</h1>;
+interface Suggestion {
+    title: string;
+    description: string;
+    likes: number;
+}
+
+type Props = {
+    suggestion: Suggestion;
 };
+
+export const SuggestionCard: React.FC<Props> = (props) => {
+    return (
+        <Container elevation={1}>
+            <Typography variant="h3">{props.suggestion.title}</Typography>
+            <TextBody variant="body1">{props.suggestion.description}</TextBody>
+            <Likes variant="body1">❤️ {props.suggestion.likes}</Likes>
+        </Container>
+    );
+};
+
+const Container = styled(Paper)`
+    display: flex;
+    padding: 16px 10px;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+
+    border-radius: 4px;
+    background: ${(props) => props.theme.palette.primary.light};
+`;
+
+const TextBody = styled(Typography)`
+    text-align: justify;
+    color: ${(props) => props.theme.palette.text.secondary};
+`;
+
+const Likes = styled(Typography)`
+    &:hover {
+        opacity: 0.8;
+        cursor: pointer;
+    }
+`;
---
+++ packages/@website/suggestion-box/components/SuggestionList.tsx
@@ -0,0 +1,59 @@
+'use client';
+import styled from '@emotion/styled';
+import { SuggestionCard } from './SuggestionCard';
+import { use, useEffect, useState } from 'react';
+import { Suggestion } from '../types/Suggestion';
+import { queryClient } from '../../../@common/ui/utils/queryClient';
+import { CircularProgress, Typography } from '@mui/material';
+
+export const SuggestionList: React.FC = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    const suggestions = use(
+        queryClient('suggestions', () =>
+            fetch(`${process.env.NEXT_PUBLIC_URL}/api/suggestions`).then(
+                (res) => res.json(),
+            ),
+        ),
+    ) as { data: Suggestion[] };
+
+    return (
+        <Container>
+            <Typography variant="h2">Sugestões</Typography>
+            {isClient ? (
+                <ContainerList>
+                    {suggestions?.data.map((suggestion, index) => (
+                        <SuggestionCard key={index} suggestion={suggestion} />
+                    ))}
+                </ContainerList>
+            ) : (
+                <CircularProgress />
+            )}
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
+`;
+
+const ContainerList = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 8px;
+
+    @media (min-width: 768px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media (min-width: 1024px) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+`;
